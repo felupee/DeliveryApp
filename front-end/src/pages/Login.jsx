45b11f545c3e8ba59be59827069c8f350a8c376a
rafaelftourinho <rafaelftourinho@gmail.com>
2023-02-27 22:41:31 -0300

    feat: req 2
    adding inputs for login and buttons

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <input type="email" data-testid="common_login__input-email" />
+      <input type="password" data-testid="common_login__input-password" />
+      <button type="submit" data-testid="common_login__button-login">
+        Entrar
+      </button>
+      <button type="submit" data-testid="common_login__button-register">
+        Registrar
+      </button>
+    </div>
   );
 }
 
