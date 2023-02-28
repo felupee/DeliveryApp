a2275b86384f1b3c83dce37222ecc2fdf5f889c8
rafaelftourinho <rafaelftourinho@gmail.com>
2023-02-27 21:03:08 -0300

    feat: req 1
    create a provider, context and router for page login

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import MainProvider from './Provider/MainProvider';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainProvider>
+      <Router>
+        <App />
+      </Router>
+    </MainProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
