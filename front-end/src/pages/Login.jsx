a9e0e2ce9bd72c28e3851d8ff5ea0c97590b095f
rafaelftourinho <rafaelftourinho@gmail.com>
2023-02-27 22:48:28 -0300

    style: format
    change the format in code on <Login />

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -3,12 +3,24 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      <input type="email" data-testid="common_login__input-email" />
-      <input type="password" data-testid="common_login__input-password" />
-      <button type="submit" data-testid="common_login__button-login">
+      <input
+        type="email"
+        data-testid="common_login__input-email"
+      />
+      <input
+        type="password"
+        data-testid="common_login__input-password"
+      />
+      <button
+        type="submit"
+        data-testid="common_login__button-login"
+      >
         Entrar
       </button>
-      <button type="submit" data-testid="common_login__button-register">
+      <button
+        type="submit"
+        data-testid="common_login__button-register"
+      >
         Registrar
       </button>
     </div>
