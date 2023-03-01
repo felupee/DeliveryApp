7b7e5a4fe009f8518d5e1326ecdf676c219859b6
rafaelmedeiross <rafael.medeiros@msn.com>
2023-03-01 11:18:21 -0300

    fix: login service

--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -5,10 +5,11 @@ const { tokenCreation } = require('../auth/jwt');
 const postLogin = async (login) => {
     const { email, password } = login;
     const userExists = await User.findOne({ where: {email}});
-    const passwordValidation = (userExists.password === md5(password));
     if (!userExists) return { code: 404};
+    const passwordValidation = (userExists.password === md5(password));
+    if (!passwordValidation) return { code: 404};
     const token = tokenCreation({ email, password });
     return { code: 200, result: { token  } };
 }
 
-module.exports = { postLogin };
+module.exports = { postLogin }
