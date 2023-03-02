6ad665b2dd8a51ba512c86b23f977b7c7289de99
Philip Lages <psilvaelages@gmail.com>
2023-03-01 21:41:42 -0300

    feat: adds handle login logic and activates cors at backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const loginRoute = require('../routes/login.route');
 const registerRoute = require('../routes/register.route');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoute);
