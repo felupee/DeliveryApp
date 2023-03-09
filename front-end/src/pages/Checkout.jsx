50ead41c136f17c7d5a3fb69f3cecd46a090e105
rafaelmedeiross <rafael.medeiros@msn.com>
2023-03-09 13:36:49 -0300

    fix: remove unused useEffect

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,20 +1,15 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import Navbar from '../Components/Navbar';
 import CheckoutList from '../Components/CheckoutList';
 import CheckoutDetails from '../Components/CheckoutDetails';
 import StorageContext from '../Context/StorageContext';
 
 export default function Checkout() {
-  const { cartStorage, setCartStorage } = useContext(StorageContext);
-  const [itens] = useState(cartStorage);
+  const { cartStorage } = useContext(StorageContext);
   const total = cartStorage.reduce((acc, item) => {
     const price = parseFloat(item.price.replace(',', '.'));
     return acc + price;
   }, 0);
-
-  useEffect(() => {
-    setCartStorage(itens);
-  }, [itens]);
 
   return (
     <>
