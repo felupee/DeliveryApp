1538385fd667068710acd6d566a44b7168e8859f
rafaelmedeiross <rafael.medeiros@msn.com>
2023-03-09 16:48:57 -0300

    fix: lint

--- front-end/src/Components/ProductList.jsx
+++ front-end/src/Components/ProductList.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ProductCard from './ProductCard';
-import './ProductList.css';
 import MainContext from '../Context/MainContext';
 
 function ProductList() {
