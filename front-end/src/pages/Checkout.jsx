7511b2758c789f188bf78424ad54f343e0b8a44f
rafaelmedeiross <rafael.medeiros@msn.com>
2023-03-09 13:46:19 -0300

    fix: total price calculation

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -7,7 +7,7 @@ import StorageContext from '../Context/StorageContext';
 export default function Checkout() {
   const { cartStorage } = useContext(StorageContext);
   const total = cartStorage.reduce((acc, item) => {
-    const price = parseFloat(item.price.replace(',', '.'));
+    const price = parseFloat(item.price.replace(',', '.')) * item.quantity;
     return acc + price;
   }, 0);
 
